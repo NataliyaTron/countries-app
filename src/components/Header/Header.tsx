f0eab4d2c44808274110fc11ce11f79b285948ba
NataliyaTron <118108299+NataliyaTron@users.noreply.github.com>
2023-11-10 18:51:22 +0400

    header router

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { UserOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import "./style.scss";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { routes } from "../../consts/routes";
 const Header = () => {
     const username = sessionStorage.getItem("username");
     const navigate = useNavigate();
@@ -11,6 +12,10 @@ const Header = () => {
         sessionStorage.removeItem("username");
         navigate("/auth");
     };
+    const links = [
+        { href: routes.MAIN, title: "Главная" },
+        { href: routes.FAVOURITE, title: "Избранное" },
+    ];
 
     return (
         <div className={"header"}>
@@ -23,6 +28,15 @@ const Header = () => {
                         />
                     </div>
                     <div className="header__actions">
+                        <ul className="header__navigation">
+                            {links.map((link) => (
+                                <li key={Math.random()}>
+                                    <NavLink to={link.href}>
+                                        {link.title}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
                         <div className="header__info">
                             <UserOutlined />
                             Ваш логин - {username}
