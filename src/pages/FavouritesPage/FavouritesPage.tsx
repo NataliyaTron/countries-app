3460f5377ab40520badf0a2e5acfbae208ee65ec
NataliyaTron <118108299+NataliyaTron@users.noreply.github.com>
2023-12-20 18:37:19 +0400

    добавила страны на страницу избранного

--- src/pages/FavouritesPage/FavouritesPage.tsx
+++ src/pages/FavouritesPage/FavouritesPage.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../toolkitRedux/store";
 
 const FavouritesPage = () => {
+    const likedCountries = useSelector(
+        (state: RootState) => state.likedCountries.likes
+    );
+
     return (
         <div>
             <h2>Избранное</h2>
+            <ul>
+                {likedCountries.map((country) => {
+                    return <li key={Math.random()}>{country}</li>;
+                })}
+            </ul>
         </div>
     );
 };
